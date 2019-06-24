44f40efa80289d42353764253e79940674d420ef
Fernando N. Frassia <ferfrassia@gmail.com>
2019-06-24 18:28:50 -0300

    Added functionality

--- Example/FFTopNotification/ViewController.swift
+++ Example/FFTopNotification/ViewController.swift
@@ -7,17 +7,12 @@
 //
 
 import UIKit
+import FFTopNotification
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func animateAction(_ sender: Any) {
+        view?.displayNotification(text: "No internet connection")
     }
 
 }
